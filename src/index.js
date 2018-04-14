f0af5bcfcccb8b352759cf74b29c37a10abc5757
Michael Gray <michael@michaelgray.info>
2018-04-14 16:31:41 +0100

    Use redux-thunk

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Immutable from 'immutable';
 
@@ -12,7 +13,7 @@ import WhichStation from './components/which_station';
 
 const useDevtools = process.env.NODE_ENV !== 'production' && typeof window.devToolsExtension === 'function';
 const createStoreWithMiddleware = compose(
-  applyMiddleware(),
+  applyMiddleware(thunk),
   useDevtools
     ? window.devToolsExtension({
         serialize: {
