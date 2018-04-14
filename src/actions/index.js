18a59f0fa0e1bd10a27b574101e6e337aa00c57e
Michael Gray <michael@michaelgray.info>
2018-04-14 18:14:52 +0100

    Fetch some data!

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,33 @@
 
 import { List } from 'immutable';
 
-import { Station, StationChoice } from '../types';
-import type { StationChoiceT } from '../types';
+import { Station, StationT, StationChoice } from '../types';
+import type { State, StationChoiceT } from '../types';
 
-export const GET_DEPARTURES = 'GET_DEPARTURES';
+import axios from 'axios';
+import ACCESS_TOKEN from '../../access_token';
 
-type ActionType = 'GET_DEPARTURES';
+export const GET_CHOICES = 'GET_CHOICES';
+export const GET_CHOICES_SUCCESS = 'GET_CHOICES_SUCCESS';
+export const GET_CHOICE_DATA = 'GET_CHOICE_DATA';
+export const GET_CHOICE_DATA_SUCCESS = 'GET_CHOICE_DATA_SUCCESS';
+
+type ActionType =
+  | 'GET_CHOICES'
+  | 'GET_CHOICES_SUCCESS'
+  | 'GET_CHOICE_DATA'
+  | 'GET_CHOICE_DATA_SUCCESS'
+  ;
 export type Action = {| type: ActionType, payload: any |};
+
+type RootState = {
+  construction: State,
+};
+type GetState = () => RootState;
+type PromiseAction = Promise<Action>;
+// eslint-disable-next-line no-use-before-define                                                                                                                                                                    
+type ThunkAction = (dispatch: Dispatch, getState: GetState) => any;
+type Dispatch = (action: Action | ThunkAction | PromiseAction | Array<Action>) => any;
 
 const parseChoices = (choices: string): List<StationChoiceT> => {
   const stationStrings = choices.split('--');
@@ -22,11 +42,50 @@ const parseChoices = (choices: string): List<StationChoiceT> => {
   return List(stations);
 }
 
+const buildQueryString = (choice: StationChoiceT, direction: string, target: StationT): string => {
+  direction = direction.toLowerCase();
+  if (direction === 'to')
+    return `/fastest/${choice.crsCode}/to/${target.crsCode}`;
+  else if (direction === 'from')
+    return `/fastest/${target.crsCode}/to/${choice.crsCode}`;
+}
+
+const decodeFastestResponse = (data: string, target: StationT) => {
+  const unavailable = { available: false, details: data };
+  if (!data.areServicesAvailable) return unavailable;
+  const service = data.departures[0].service;
+  if (!service) return unavailable;
+  const targetCrs = target.crsCode.toUpperCase();
+  const targetDetails = service.subsequentCallingPoints[0].callingPoint.find(cp => cp.crs.toUpperCase() === targetCrs);
+  if (!targetDetails) return unavailable;
+  return { available: true, eta: targetDetails.st, details: targetDetails };
+}
+
+const ROOT_URL = 'https://huxley.apphb.com';
+console.log('ACCESS_TOKEN', ACCESS_TOKEN);
+
+const getFastest = (dispatch: Dispatch, index, choice: StationChoiceT, direction: string, target: StationT) => {
+  dispatch({ type: GET_CHOICE_DATA, payload: { index } });
+  const queryString = buildQueryString(choice, direction, target);
+  return axios.get(`${ROOT_URL}${queryString}?expand=true&accessToken=${ACCESS_TOKEN}`)
+    .then(request => dispatch({
+      type: GET_CHOICE_DATA_SUCCESS,
+      payload: { index, result: decodeFastestResponse(request.data, target) }, 
+    }))
+}
+
 export function getDepartures(choices: string, direction: string, target: string) {
   const stationChoices = parseChoices(choices);
   const stationTarget = Station({crsCode: target});
-  return {
-    type: GET_DEPARTURES,
-    payload: { choices: stationChoices, direction, target: stationTarget },
+  return (dispatch: Dispatch) => {
+    dispatch({
+        type: GET_CHOICES,
+        payload: { choices: stationChoices, direction, target: stationTarget },
+    });
+    Promise.all(
+      stationChoices.map((choice, index) => getFastest(dispatch, index, choice, direction, stationTarget))
+    ).then(
+        () => dispatch({type: GET_CHOICES_SUCCESS, payload: {} })
+      );
   };
 }
--- src/reducers/stations.js
+++ src/reducers/stations.js
@@ -6,7 +6,7 @@ import { Station } from '../types'
 import type { State } from '../types';
 import type { Action } from '../actions';
 
-import { GET_DEPARTURES } from '../actions';
+import { GET_CHOICES } from '../actions';
 
 const initialState: State = Record({
   choices: List(),
@@ -17,7 +17,7 @@ const initialState: State = Record({
 export default function(state: State = initialState, action: Action) {
     console.log(state, action);
   switch (action.type) {
-  case GET_DEPARTURES:
+  case GET_CHOICES:
     return state.merge(action.payload);
   default:
     return state;
