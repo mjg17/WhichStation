3880b2b552f05b220dff74619f47ec92f9b8deaa
Michael Gray <michael@michaelgray.info>
2018-04-15 09:53:35 +0100

    Reducer for GET_CHOICE_DATA and GET_CHOICES_SUCCESS

--- src/reducers/stations.js
+++ src/reducers/stations.js
@@ -6,19 +6,38 @@ import { Station } from '../types'
 import type { State } from '../types';
 import type { Action } from '../actions';
 
-import { GET_CHOICES } from '../actions';
+import { GET_CHOICES, GET_CHOICE_DATA, GET_CHOICE_DATA_SUCCESS, GET_CHOICES_SUCCESS } from '../actions';
+import { compareHHMM } from '../utils/date';
 
 const initialState: State = Record({
   choices: List(),
   direction: 'to',
   target: Station({}),
+  loading: false,
 })();
 
 export default function(state: State = initialState, action: Action) {
     console.log(state, action);
   switch (action.type) {
   case GET_CHOICES:
-    return state.merge(action.payload);
+    return state.set('loading', true).merge(action.payload);
+  case GET_CHOICE_DATA: {
+    const { index } = action.payload;
+    return state.setIn(['choices', index, 'loading'], true);
+  }
+  case GET_CHOICE_DATA_SUCCESS: {
+    const { index, result } = action.payload;
+    return state.updateIn(
+      ['choices', index],
+      c => c.set('loading', false).merge(result)
+    );
+  }
+  case GET_CHOICES_SUCCESS: {
+    // sort choices by arrival time
+    return state
+      .set('loading', false)
+      .update('choices', c => c.sort((a, b) => compareHHMM(a.arr, b.arr)));
+  }
   default:
     return state;
   }
--- src/types.js
+++ src/types.js
@@ -9,18 +9,38 @@ export type StationProps = {|
 |};
 export type StationT = RecordOf<StationProps>;
 
-export const Station: RecordFactory<StationProps> = Record({crsCode: '', stationName: ''}, 'Station');
+export const Station: RecordFactory<StationProps> = Record({
+  crsCode: '',
+  stationName: '',
+}, 'Station');
 
 export type StationChoiceProps = {|
   ...StationProps,
   travelMinutes: number,
+  loading: boolean,
+  available: boolean,
+  arr: string,
+  dep: string,
+  sta: string,
+  std: string,
 |};
 export type StationChoiceT = RecordOf<StationChoiceProps>;
 
-export const StationChoice: RecordFactory<StationChoiceProps> = Record({crsCode: '', stationName: '', travelMinutes: 0}, 'StationChoice');
+export const StationChoice: RecordFactory<StationChoiceProps> = Record({
+  crsCode: '',
+  stationName: '',
+  travelMinutes: 0,
+  loading: false,
+  available: false,
+  arr: '',
+  dep: '',
+  sta: '',
+  std: '',
+}, 'StationChoice');
 
 export type State = RecordOf<{
     choices: List<StationChoiceT>,
     direction: string,
     target: StationT,
+    loading: boolean,
 }>;
